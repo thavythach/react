fa9a23c462f1b9ad1f61a052a3dfb649422c1a3c
Thavy Thach <thavythach@gmail.com>
2019-11-29 21:47:54 -0600

    search

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
     super();
     
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
@@ -21,6 +22,11 @@ class App extends Component {
   render(){
       return (
         <div className="App">
+          <input 
+            type='search' 
+            placeholder='search monsters' 
+            onChange={e => this.setState({ searchField: e.target.value }) }
+            />
           <CardList monsters={this.state.monsters} />
         </div>
       );
