09879abbf3d56a75a4e332f7dd3544eace8acbf2
Thavy Thach <thavythach@gmail.com>
2019-11-30 10:41:14 -0600

    Updates

---
+++ precache-manifest.9e82086b441381ea97f05a10efac1b8c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e81917703658c8059b90bf98c9b864",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "7a8351d38f32483c48df",
+    "url": "/react/static/css/main.7ebfce57.chunk.css"
+  },
+  {
+    "revision": "22555f7726a5e4b4c19a",
+    "url": "/react/static/js/2.77688bae.chunk.js"
+  },
+  {
+    "revision": "7a8351d38f32483c48df",
+    "url": "/react/static/js/main.d41e9990.chunk.js"
+  },
+  {
+    "revision": "846590ac0e71448f5007",
+    "url": "/react/static/js/runtime-main.6368bd89.js"
+  }
+]);
