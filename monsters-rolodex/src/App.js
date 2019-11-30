50765078a7b5311eeaed332248551a3b74c6fb48
Thavy Thach <thavythach@gmail.com>
2019-11-29 21:24:56 -0600

    breaking these into components

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -12,31 +12,16 @@ class App extends Component {
     };
   }
 
-
-
   componentDidMount(){
     fetch('http://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({monsters: users}));
   }
-    
-
-
   
   render(){
       return (
         <div className="App">
-        <CardList name="Thavy">
-        
-        {
-          this.state.monsters.map(
-            monster => (
-              <h1 key={monster.id}> { monster.name } </h1>
-            )
-          )
-        }
-        </CardList>
-          
+          <CardList monsters={this.state.monsters} />
         </div>
       );
   }
