02db7e2493890dac71621f88ff7049b743262e72
Thavy Thach <thavythach@gmail.com>
2019-12-02 20:23:23 -0600

    reusable butttttton

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends React.Component {
     constructor (props) {
@@ -34,7 +35,7 @@ class SignIn extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <FormInput name="email" type="email" value={this.state.email} handleChange={this.handleChange} label="email" required />
                     <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
-                    <input type='submit' value='Submit Form' />
+                    <CustomButton type='submit'>Sign In</CustomButton>
                 </form>
             </div>
 
