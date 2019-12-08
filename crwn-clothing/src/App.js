d845c3ffe0078da0e516fa8a5def508c8c9a727f
Thavy Thach <thavythach@gmail.com>
2019-12-08 14:30:50 -0600

    space

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -41,7 +41,7 @@ class App extends React.Component {
           <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
-            <Route path='/signin' component={SignInAndSignUpPage} />
+            <Route path='/signin' component={SignInAndSignUpPage}/>
           </Switch>
         </div>
       );
